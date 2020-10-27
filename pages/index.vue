6af819c557b1cfdba81855ed9917f00aeb2139fc
davies-maina <daviesmaina@outlook.com>
2020-10-27 13:21:51 +0300

    api calls with axios + error message when server is down

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from "../components/EventCard";
 export default {
   head() {
     // <-- property used by vue-meta to add header tags
@@ -12,5 +19,25 @@ export default {
       title: "Event Listing", // <-- For our title tag
     };
   },
+
+  asyncData(context, error) {
+    return context.$axios
+      .get("http://localhost:8000/events")
+      .then((res) => {
+        return {
+          events: res.data,
+        };
+      })
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message: "unable to fetch events at this time. Please try again",
+        });
+      });
+  },
+  components: {
+    EventCard,
+  },
 };
 </script>
+
